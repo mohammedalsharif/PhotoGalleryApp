58d9d456635d9eeb6c592821a8fdd147f1362034
mohammed al sharif <ffmm972006@gmail.com>
2022-10-01 21:09:24 +0300

    init commit

---
+++ app/src/main/java/com/examples/photogalleryapp/ui/Home/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.examples.photogalleryapp.ui.Home
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.navigation.NavController
+import androidx.navigation.NavDestination
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import com.examples.photogalleryapp.R
+import com.examples.photogalleryapp.databinding.ActivityMainBinding
+import com.examples.photogalleryapp.databinding.FragmentHomeBinding
+
+class MainActivity : AppCompatActivity() {
+    private  val TAG = "MainActivity"
+    lateinit var binding: ActivityMainBinding
+    lateinit var navController: NavController
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding=ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        navController=Navigation.findNavController(findViewById(R.id.fragment))
+        navController.addOnDestinationChangedListener(listener = NavController.OnDestinationChangedListener(
+                function= { controller: NavController, destination: NavDestination, arguments: Bundle? ->
+                    when(destination.id){
+                        R.id.homeFragment->{
+                            Log.e(TAG, "onCreate: ", )
+                            Toast.makeText(this,"fragment",Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                }
+            ))
+    }
+}
